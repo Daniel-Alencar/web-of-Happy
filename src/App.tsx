76db02bb0715b072cbee6574a64726f2f32f5ed8
Daniel-Alencar <danielalencar746@gmail.com>
2020-10-21 12:29:13 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+// .JSX - JAVASCRIPT XML
+/*
+  É uma maneira de colocar html dentro do javascript
+*/
+// .TSX - TYPESCRIPT XML
+/*
+  É uma maneira de colocar html dentro do typescript
+*/
+
+// interface somente no typescript
+interface Nomepropriedades {
+  text: string;
+}
+
+function Name(propriedades: Nomepropriedades) {
+  return <h1>{propriedades.text}</h1>
+}
+
 function App() {
-  return (
+  return ( // className funciona da mesma forma que uma class (não pode ser class porque é uma palavra reservada do JS)
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Welcome to NLW,
+        <Name text="Daniel"/>,
+        <Name text="Maria"/>,
+        <Name text="Vitor"/> e <Name text="Jéssica"/>
+      </h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/**
+ * Importamos o react + a integração com a DOM do nosso html
+ */
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <App/>, //Aqui entra o que será redirecionado para o html final da página
+  document.getElementById('root') // Onde será colocado o html que nós queremos
+); 
